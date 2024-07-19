ea1e55251d5f3e1993557bd2ace9bf73a772035d
Prem Modha <premmodhaofficial@gmail.com>
2024-07-19 09:11:35 +0530

    undotree

--- nvim/.config/nvim/lua/custom/plugins/undootree.lua
+++ nvim/.config/nvim/lua/custom/plugins/undootree.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+	"mbbill/undotree",
+	-- undotree = require("undotree"),
+	config = function()
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		vim.g.undotree_WindowLayout = 2
+		vim.g.undotree_SetFocusWhenToggle = 1
+	end,
+}
