57258639d583a16351cb50d581b9b62674716bef
Prem Modha <premmodhaofficial@gmail.com>
2024-07-25 12:31:38 +0530

    diognostic 0

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -51,6 +51,7 @@ Kickstart Guide:
       - Tutor
       - <enter key>
 
+
     (If you already know the Neovim basics, you can skip this step.)
 
   Once you've completed that, you can continue working through **AND READING** the rest
@@ -286,20 +287,15 @@ require("lazy").setup({
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", "[C]ode" },
+				{ "<leader>d", "[D]ocument" },
+				{ "<leader>r", "[R]ename" },
+				{ "<leader>s", "[S]earch" },
+				{ "<leader>w", "[W]orkspace" },
+				{ "<leader>t", "[T]oggle" },
+				{ "<leader>h", "Git [H]unk", mode = { "n", "v" } },
 			})
-
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
 		end,
 	},
 
@@ -568,6 +564,8 @@ require("lazy").setup({
 		end,
 	},
 
+	-- neovide
+
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
@@ -639,3 +637,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 		})
 	end,
 })
+if vim.g.neovide then
+	vim.o.guifont = "JetBrainsMono NF:h18" -- text below applies for VimScript
+end
