d9c914de4cf1b938e451a0442979e3706076eb09
Prem Modha <premmodhaofficial@gmail.com>
2024-08-18 00:24:24 +0530

    python with django finaly works damn!

---
+++ nvim/.config/nvim/lua/custom/plugins/pythonyeahiknow.lua
@@ -0,0 +1,10 @@
+return {
+	"HallerPatrick/py_lsp.nvim",
+	config = function()
+		require("py_lsp").setup({
+			-- This is optional, but allows to create virtual envs from nvim
+			host_python = "/usr/bin/python3",
+			default_venv_name = ".venv", -- For local venv
+		})
+	end,
+}
