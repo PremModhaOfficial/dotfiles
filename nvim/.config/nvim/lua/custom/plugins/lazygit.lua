772912bc782e63777a4beffa704a7fa12c589692
Prem Modha <premmodhaofficial@gmail.com>
2024-08-18 00:25:00 +0530

    lazygit and git sign but i can blame and diffffff
    sqush me!

--- nvim/.config/nvim/lua/custom/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/custom/plugins/lazygit.lua
@@ -11,10 +11,13 @@ return {
 	-- optional for floating window border decoration
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"lewis6991/gitsigns.nvim",
 	},
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		{ "<leader>gu", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		{ "<leader>gd", "<cmd>Gitsigns preview_hunk<cr>", desc = "show line diff (Gitsigns)" },
+		{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Gitsigns toggle current line blame" },
 	},
 }
