204e77bd8eefc93bbfc2feebc78769e373250be7
Prem Modha <premmodhaofficial@gmail.com>
2024-08-21 08:42:37 +0530

    some new colors

--- nvim/.config/nvim/lua/custom/plugins/colorSchemes.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorSchemes.lua
@@ -1,5 +1,33 @@
 return {
-
+	{
+		"tiagovla/tokyodark.nvim",
+		opts = {
+			transparent_background = true, -- set background to transparent
+			gamma = 1.00, -- adjust the brightness of the theme
+			styles = {
+				comments = { italic = true }, -- style for comments
+				keywords = { italic = true }, -- style for keywords
+				identifiers = { italic = true }, -- style for identifiers
+				functions = {}, -- style for functions
+				variables = {}, -- style for variables
+			},
+			---@diagnostic disable-next-line: unused-local
+			custom_highlights = {} or function(highlights, palette)
+				return {}
+			end, -- extend highlights
+			---@diagnostic disable-next-line: unused-local
+			custom_palette = {} or function(palette)
+				return {}
+			end, -- extend palette
+			terminal_colors = true, -- enable terminal colors
+		},
+		config = function(_, opts)
+			require("tokyodark").setup(opts) -- calling setup is optional
+		end,
+		init = function()
+			vim.cmd([[colorscheme tokyodark]])
+		end,
+	},
 	{ -- You can easily change to a different colorscheme.
 		-- Change the name of the colorscheme plugin below, and then
 		-- change the command in the config to whatever the name of that colorscheme is.
@@ -7,17 +35,18 @@ return {
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
-		--[[
-		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			-- vim.cmd.colorscheme("tokyonight-night")
-
-			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+		config = function()
+			require("tokyonight").setup()
 		end,
-		--]]
+		-- init = function()
+		-- 	-- Load the colorscheme here.
+		-- 	-- Like many other themes, this one has different styles, and you could load
+		-- 	-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+		-- 	vim.cmd.colorscheme("tokyonight-night")
+		--
+		-- 	-- You can configure highlights by doing something like:
+		-- 	vim.cmd.hi("Comment gui=none")
+		-- end,
 	},
 
 	{
@@ -49,9 +78,6 @@ return {
 				transparent_mode = true,
 			})
 		end,
-		init = function()
-			vim.cmd.colorscheme("gruvbox")
-			-- vim.cmd.hi("Comment gui=none")
-		end,
+		-- init = function() vim.cmd.colorscheme("gruvbox") -- vim.cmd.hi("Comment gui=none") end,
 	},
 }
