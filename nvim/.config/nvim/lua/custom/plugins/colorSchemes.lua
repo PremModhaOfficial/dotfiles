86426e938ecafec546b4e0cfff813e76d6298542
Prem Modha <premmodhaofficial@gmail.com>
2024-08-27 23:26:52 +0530

    colors

--- nvim/.config/nvim/lua/custom/plugins/colorSchemes.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorSchemes.lua
@@ -1,4 +1,77 @@
+ColorScheme = "tokyodark"
 return {
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[ set background=dark ]])
+			require("cyberdream").setup({
+				-- Enable transparent background
+				transparent = true,
+				-- Enable italics comments
+				italic_comments = true,
+				-- Replace all fillchars with ' ' for the ultimate clean look
+				hide_fillchars = true,
+				-- Modern borderless telescope theme - also applies to fzf-lua
+				borderless_telescope = false,
+				-- Set terminal colors used in `:terminal`
+				terminal_colors = true,
+				-- Use caching to improve performance - WARNING: experimental feature - expect the unexpected!
+				-- Early testing shows a 60-70% improvement in startup time. YMMV. Disables dynamic light/dark theme switching.
+				cache = true, -- generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+				theme = {
+					variant = "auto", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+					highlights = {
+						-- Highlight groups to override, adding new groups is also possible
+						-- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+						-- Example:
+						Comment = { fg = "#696969", bg = "NONE", italic = true },
+						-- Complete list can be found in `lua/cyberdream/theme.lua`
+					},
+					-- Override a highlight group entirely using the color palette
+					-- overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+					--     -- Example:
+					--     return {
+					--         Comment = { fg = colors.green, bg = "NONE", italic = true },
+					--         ["@property"] = { fg = colors.magenta, bold = true },
+					--     }
+					-- end,
+					-- Override a color entirely
+					-- 	colors = { -- For a list of colors see `lua/cyberdream/colours.lua` Example: bg = "#000000", green = "#00ff00", magenta = "#ff00ff", },
+				},
+			})
+		end,
+		init = function()
+			if ColorScheme == "cyberdream" then
+				vim.cmd([[
+				colorscheme cyberdream
+				CyberdreamBuildCache
+				]])
+			end
+		end,
+	},
+	{
+		"maxmx03/fluoromachine.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local fm = require("fluoromachine")
+
+			fm.setup({
+				glow = false,
+				theme = "fluoromachine",
+				transparent = true,
+				brightness = 0.05,
+				true_colors = true,
+			})
+		end,
+		init = function()
+			if ColorScheme == "fluoromachine" then
+				vim.cmd([[colorscheme fluoromachine]])
+			end
+		end,
+	},
 	{
 		"tiagovla/tokyodark.nvim",
 		opts = {
@@ -10,6 +83,7 @@ return {
 				identifiers = { italic = true }, -- style for identifiers
 				functions = {}, -- style for functions
 				variables = {}, -- style for variables
+				folds = { bold = true, italic = true }, -- style for folds
 			},
 			---@diagnostic disable-next-line: unused-local
 			custom_highlights = {} or function(highlights, palette)
@@ -25,7 +99,9 @@ return {
 			require("tokyodark").setup(opts) -- calling setup is optional
 		end,
 		init = function()
-			vim.cmd([[colorscheme tokyodark]])
+			if ColorScheme == "tokyodark" then
+				vim.cmd([[colorscheme tokyodark]])
+			end
 		end,
 	},
 	{ -- You can easily change to a different colorscheme.
@@ -38,15 +114,17 @@ return {
 		config = function()
 			require("tokyonight").setup()
 		end,
-		-- init = function()
-		-- 	-- Load the colorscheme here.
-		-- 	-- Like many other themes, this one has different styles, and you could load
-		-- 	-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		-- 	vim.cmd.colorscheme("tokyonight-night")
-		--
-		-- 	-- You can configure highlights by doing something like:
-		-- 	vim.cmd.hi("Comment gui=none")
-		-- end,
+		init = function()
+			if ColorScheme == "tokyonight" then
+				-- Load the colorscheme here.
+				-- Like many other themes, this one has different styles, and you could load
+				-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+				vim.cmd.colorscheme("tokyonight-night")
+
+				-- You can configure highlights by doing something like:
+				vim.cmd.hi("Comment gui=none")
+			end
+		end,
 	},
 
 	{
@@ -78,6 +156,11 @@ return {
 				transparent_mode = true,
 			})
 		end,
-		-- init = function() vim.cmd.colorscheme("gruvbox") -- vim.cmd.hi("Comment gui=none") end,
+		init = function()
+			if ColorScheme == "gruvbox" then
+				vim.cmd([[colorscheme gruvbox]])
+				vim.cmd.hi("Comment gui=none")
+			end
+		end,
 	},
 }
