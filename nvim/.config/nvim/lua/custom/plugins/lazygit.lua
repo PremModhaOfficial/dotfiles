d773c4f2fed8cde718944628ad6d3cccd95b73a5
Prem Modha <premmodhaofficial@gmail.com>
2024-08-30 18:51:18 +0530

    added magit style replacing lazygit

--- nvim/.config/nvim/lua/custom/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/custom/plugins/lazygit.lua
@@ -1,23 +1,48 @@
 -- nvim v0.8.0
 return {
-	"kdheepak/lazygit.nvim",
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = function()
+			require("neogit").setup()
+			require("which-key").add({
+				{
+					"<leader>gm",
+					"<cmd>Neogit<cr>",
+					desc = "Neogit",
+				},
+			})
+		end,
 	},
-	-- optional for floating window border decoration
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"lewis6991/gitsigns.nvim",
-	},
-	-- setting the keybinding for LazyGit with 'keys' is recommended in
-	-- order to load the plugin when the command is run for the first time
-	keys = {
-		{ "<leader>gu", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		{ "<leader>gc", "<cmd>Gitsigns preview_hunk<cr>", desc = "show line diff (Gitsigns)" },
-		{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Gitsigns toggle current line blame" },
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"lewis6991/gitsigns.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>gu", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			{ "<leader>gc", "<cmd>Gitsigns preview_hunk<cr>", desc = "show line diff (Gitsigns)" },
+			{
+				"<leader>gb",
+				"<cmd>Gitsigns toggle_current_line_blame<cr>",
+				desc = "Gitsigns toggle current line blame",
+			},
+		},
 	},
 }
