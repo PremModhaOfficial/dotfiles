16c02db70784f99eca1408006d085fc1f5e69d8d
Prem Modha <premmodhaofficial@gmail.com>
2024-09-16 23:06:40 +0530

    removed eslint_d cousing problems

--- nvim/.config/nvim/lua/custom/plugins/THENONELS.lua
+++ nvim/.config/nvim/lua/custom/plugins/THENONELS.lua
@@ -7,7 +7,6 @@ return {
 			source = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.black.with({
 					extra_args = { "--line-length", "80" },
 				}),
