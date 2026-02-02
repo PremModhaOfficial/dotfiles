8c2447594558c4d552cc39801289f73051f4b9e0
prem modha <prem.modha@motadata.com>
2026-02-02 19:37:42 +0530

    feat: add fillchar transparency (Whitespace, NonText, EndOfBuffer) to make_none()

--- nvim/.config/nvim/lua/lib/utils.lua
+++ nvim/.config/nvim/lua/lib/utils.lua
@@ -4,15 +4,24 @@ local function make_none()
 	local Normal = vim.api.nvim_get_hl(0, { name = "Normal" })
 	local NormalNC = vim.api.nvim_get_hl(0, { name = "NormalNC" })
 	local NormalFloat = vim.api.nvim_get_hl(0, { name = "NormalFloat" })
+	local Whitespace = vim.api.nvim_get_hl(0, { name = "Whitespace" })
+	local NonText = vim.api.nvim_get_hl(0, { name = "NonText" })
+	local EndOfBuffer = vim.api.nvim_get_hl(0, { name = "EndOfBuffer" })
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Whitespace", { fg = "none", bg = "none" })
+	vim.api.nvim_set_hl(0, "NonText", { fg = "none", bg = "none" })
+	vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "none", bg = "none" })
 
 	return function()
 		vim.api.nvim_set_hl(0, "Normal", { bg = Normal.bg })
 		vim.api.nvim_set_hl(0, "NormalNC", { bg = NormalNC.bg })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = NormalFloat.bg })
+		vim.api.nvim_set_hl(0, "Whitespace", { fg = Whitespace.fg, bg = Whitespace.bg })
+		vim.api.nvim_set_hl(0, "NonText", { fg = NonText.fg, bg = NonText.bg })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = EndOfBuffer.fg, bg = EndOfBuffer.bg })
 	end
 end
 
