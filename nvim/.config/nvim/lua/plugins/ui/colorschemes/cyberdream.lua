4153f4f7748b7d2a77356b332bcd0661bbd60ffb
prem modha <prem.modha@motadata.com>
2026-02-02 19:29:12 +0530

    feat: restore cyberdream with gotham256 color palette

--- nvim/.config/nvim/lua/plugins/ui/colorschemes/cyberdream.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorschemes/cyberdream.lua
@@ -1,38 +1,40 @@
 return {
 	"scottmckendry/cyberdream.nvim",
 	lazy = false,
-	enabled = false,
+	enabled = true,
 	priority = 1000,
 	config = function()
 		require("cyberdream").setup({
-			-- Set light or dark variant
 			variant = "auto",
-			-- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background` Enable transparent background
 			transparent = true,
-			-- Reduce the overall saturation of colours for a more muted look
-			-- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default) Enable italics comments
-			saturation = 0.5,
-			italic_comments = false, -- Replace all fillchars with ' ' for the ultimate clean look
-			hide_fillchars = true, -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
-			borderless_pickers = true, -- Set terminal colors used in `:terminal`
+			saturation = 1,
+			italic_comments = false,
+			hide_fillchars = true,
+			borderless_pickers = true,
 			terminal_colors = true,
-
-			-- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
 			cache = false,
 
-			-- Override highlight groups with your own colour values
-			highlights = {
-				Comment = { fg = "#696969", bg = "NONE", italic = true },
-
-				BlinkCmpMenu = { bg = "#16181a" },
-				BlinkCmpMenuBorder = { bg = "#16181a", fg = "#00d9ff" },
-				BlinkCmpDoc = { bg = "#16181a" },
-				BlinkCmpDocBorder = { bg = "#16181a", fg = "#00d9ff" },
-				BlinkCmpSignatureHelp = { bg = "#16181a" },
-				BlinkCmpSignatureHelpBorder = { bg = "#16181a", fg = "#00d9ff" },
+			colors = {
+				bg = "#0c1014",
+				bg_alt = "#11151c",
+				bg_highlight = "#0a3749",
+				fg = "#d3ebe9",
+				grey = "#599cab",
+				red = "#c23127",
+				orange = "#d26937",
+				yellow = "#edb443",
+				green = "#2aa889",
+				cyan = "#33859E",
+				blue = "#195466",
+				magenta = "#888ca6",
+				pink = "#888ca6",
+				purple = "#4e5166",
 			},
 
-			-- Disable or enable colorscheme extensions
+			highlights = {
+				Comment = { fg = "#696969", bg = "NONE", italic = true },
+			},
+
 			extensions = {
 				blinkcmp = true,
 				dapui = true,
@@ -54,17 +56,7 @@ return {
 			},
 		})
 
-		-- Set the colorscheme
 		vim.cmd("colorscheme cyberdream")
-
-		local get_hl = vim.api.nvim_get_hl
-		local set_hl = vim.api.nvim_set_hl
-
-		local menu_bg = get_hl(0, { name = "BlinkCmpMenu" }).bg or "#16181a"
-		local primary_color = "#00d9ff"
-
-		set_hl(0, "BlinkCmpMenuBorder", { bg = menu_bg, fg = primary_color })
-		set_hl(0, "BlinkCmpDocBorder", { bg = menu_bg, fg = primary_color })
-		set_hl(0, "BlinkCmpSignatureHelpBorder", { bg = menu_bg, fg = primary_color })
+		require("lib.utils").set_transparent_special_chars()
 	end,
 }
