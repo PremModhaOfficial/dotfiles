4d1fc5b5be087766bfcef5cc5e7904f55e378b2b
prem modha <prem.modha@motadata.com>
2026-08-10 13:10:00 +0530

    chore(term): update ghostty theme and kitty/wezterm fonts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,8 +6,11 @@ local config = wezterm.config_builder()
 config.enable_wayland = true
 config.enable_tab_bar = false
 config.font = wezterm.font_with_fallback({
-	"CaskaydiaCove Nerd Font",
+	"IosevkaTerm Nerd Font Mono",
+	"ZedMono Nerd Font Mono",
+	"VictorMono Nerd Font Mono",
 	"Noto Color Emoji",
+	"WenQuanYi Zen Hei",
 })
 
 config.keys = {
